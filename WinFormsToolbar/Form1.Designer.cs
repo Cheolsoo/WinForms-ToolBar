62272ec1e6402fc939e30df0cadda83cdf977958
cheolsoo kim <nabiro@gmail.com>
2024-06-23 22:20:06 +0900

    6테스트 -> 7테스트

--- WinFormsToolbar/Form1.Designer.cs
+++ WinFormsToolbar/Form1.Designer.cs
@@ -239,7 +239,7 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(132, 12);
             this.label4.TabIndex = 17;
-            this.label4.Text = "Button change6 테스트";
+            this.label4.Text = "Button change7 테스트";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // menuStrip1
